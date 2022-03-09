2a82169d931e95b97c566e23b5ba7f19d1bbcb23
Giovanni Fersan <giovanni.santos@meta.com.br>
2022-03-08 22:07:25 -0300

    Mudanças no início e adição de Header nos componentes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,34 @@
-import styles from './Header.module.scss'
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Header.module.scss";
 
-export function Header (){
+export function Header() {
   return (
-    <h1>Header</h1>
-  )
+    <>
+      <nav className={styles.nav}>
+      <a className={styles.nav_logo}>
+        <Link href="/" passHref>
+          <Image src="/vercel.svg" alt="logo" width={200} height={200} />
+        </Link>
+      </a>
+        <ul className={styles.nav_menu}>
+          <li>
+            <Link href="/">
+              <a>Início</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>Sobre</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contato</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
---
+++ src/components/HomeSection/index.tsx
@@ -0,0 +1,7 @@
+export default function HomeSection(){
+    return (
+        <>
+        <h1>Primeira section</h1>
+        </>
+    )
+}
